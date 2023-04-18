5ee099e9ce3d87432482ad37df3430612bdd1301
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-04-18 09:39:55 +0700

    fix cls

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="hero w-[100vw] h-[100vh] lg:px-[150px] lg:py-[150px] md:px-[50px] md:py-[50px] px-[20px] pt-[100px]">
     <ClientOnly>
-      <h1 class="font-semibold lg:text-[40px] text-[27px] text-gray-900 mb-4">
-        {{ $t('welcome') }}
-      </h1>
-      <h2 class="font-semibold text-[20px] lg:text-[32px] text-gray-900 mt-2 mb-8">
-        <div class="flex items-center gap-2">I am
-          <VueWriter class="underline text-orange-600"
-            :array="['Programmer', 'Photography Enthusiast', 'Freelance Programmer']" caret="underscore" :typeSpeed="90"
-            :eraseSpeed="50" />
-        </div>
-      </h2>
+      <div class="max-w-[500px] max-h-[500px]">
+        <h1 class="font-semibold lg:text-[40px] text-[27px] text-gray-900 mb-4">
+          {{ $t('welcome') }}
+        </h1>
+        <h2 class="font-semibold text-[20px] lg:text-[32px] text-gray-900 mt-2 mb-8">
+          <div class="flex items-center gap-2">I am
+            <VueWriter class="underline text-orange-600"
+              :array="['Programmer', 'Photography Enthusiast', 'Freelance Programmer']" caret="underscore" :typeSpeed="90"
+              :eraseSpeed="50" />
+          </div>
+        </h2>
+      </div>
     </ClientOnly>
 
     <ButtonNext to="/about" name="start" />
