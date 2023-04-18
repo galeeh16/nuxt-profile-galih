74d4e7b4edcf2bd30c354faaa0b6fa2021bd403a
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-04-18 09:31:27 +0700

    add head meta and lang

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="hero w-[100vw] h-[100vh] lg:px-[150px] lg:py-[150px] md:px-[50px] md:py-[50px] px-[20px] pt-[100px]">
     <ClientOnly>
-      <h1 class="font-semibold lg:text-[40px] text-[27px] text-gray-900 mb-4">
+      <p class="font-semibold lg:text-[40px] text-[27px] text-gray-900 mb-4">
         {{ $t('welcome') }}
-      </h1>
+      </p>
       <h2 class="font-semibold text-[20px] lg:text-[32px] text-gray-900 mt-2 mb-8">
         <div class="flex items-center gap-2">I am
           <VueWriter class="underline text-orange-600"
